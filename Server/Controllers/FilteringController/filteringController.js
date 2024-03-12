c744b82d794cae5ea762f3d182b76e31b39d157b
Bikram-Gyawali <bikramgyawali57@gmail.com>
2024-03-12 21:54:17 +0545

    some changes in code

---
+++ Server/Controllers/FilteringController/filteringController.js
@@ -0,0 +1,12 @@
+const Job = require("../../Models/JobModel");
+const Candidates = require("../../Models/Candidate");
+
+const showActiveJobs = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = showActiveJobs;
