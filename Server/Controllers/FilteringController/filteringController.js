00961e9e838d0106b4401bd25dc9ab28e01a8cfd
Bikram-Gyawali <bikramgyawali57@gmail.com>
2024-03-13 22:00:51 +0545

    implementation of filtering algorithm controller

--- Server/Controllers/FilteringController/filteringController.js
+++ Server/Controllers/FilteringController/filteringController.js
@@ -1,12 +1,27 @@
 const Job = require("../../Models/JobModel");
 const Candidates = require("../../Models/Candidate");
 
-const showActiveJobs = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const getAIFilteredCandidates = async (req, res, next) => {
+  try {
+    const jobId = req.body.jobId;
 
-module.exports = showActiveJobs;
+    const job = await Job.findById(jobId);
+
+    if (!job) {
+      return res.status(400).json({ error: "Job not found" });
+    }
+
+    const appliedCandidates = await Candidates.find({ jobId: jobId }).lean();
+
+    const jobDescription = job.job_description;
+    const resumeURL = appliedCandidates.map((candidate) => {
+      return candidate.ResumeURL;
+    });
+
+    // implement cosine similarity or ifdif alogorithm for filtering out the candidates based on their resumes comparing with job description
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getAIFilteredCandidates;
