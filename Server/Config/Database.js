872eba6b819835bc51d2ba9b25ef6bc7d5e1d5ba
Bikram-Gyawali <bikramgyawali57@gmail.com>
2024-04-22 23:33:05 +0545

    removed unused codes

--- Server/Config/Database.js
+++ Server/Config/Database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 const connection = async () => {
     try {
         await mongoose.connect(process.env.MONGOURL);
+        console.log("Database connected");
     }
+
     catch (e) {
         console.log("DB Error: " + e);
     }
